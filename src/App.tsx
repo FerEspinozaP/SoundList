7724b3379e04c612c1f769df2aa54e34c67d2d0f
FerEspinozaP <71610246+FerEspinozaP@users.noreply.github.com>
2021-11-29 22:14:31 -0600

    modify files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React from 'react';
+import { IonApp, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
 
-/* Core CSS required for Ionic components to work properly */
+/* Core CSS required for Io nic components to work properly */
 import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
@@ -24,16 +22,11 @@ import './theme/variables.css';
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>SoundList</IonTitle>
+      </IonToolbar>
+    </IonHeader>
   </IonApp>
 );
 
